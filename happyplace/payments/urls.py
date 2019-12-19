724a54cded23e9143b9df32eaf46c934088849d7
Rifat Hasan <piyalr04@gmail.com>
2019-12-19 23:52:11 +0600

    updated files

--- happyplace/payments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-# url paths for payments
-urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('charge/', views.charge, name='charge'),
-]
--- happyplace/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from users import views
-
-#  Url paths of the website with naming
-urlpatterns = [
-    path('profile/', views.profile, name='user-profile'),
-    ]
-
-
